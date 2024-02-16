4513450d02e948dbd36e273b68406c4f429c2e6e
artprange <arthur.prange@gmail.com>
2024-02-16 20:09:40 -0300

    remaining bugs cleared! rock and roll!

--- igniteTeams/babel.config.js
+++ igniteTeams/babel.config.js
@@ -1,25 +1,24 @@
-module.exports = function (api) {
+module.exports = function(api) {
 	api.cache(true);
 	return {
-		presets: ["babel-preset-expo"],
-		plugins: [
-			[
-				"module-resolver",
-				{
-					root: ["./src"],
-					alias: {
-						"@assets": "./src/assets",
-						"@components": "./src/components",
-						"@routes": "./src/routes",
-						"@screens": "./src/screens",
-						"@storage": "./src/storage",
-						"@utils": "./src/utils",
-					},
-				},
-			],
-		],
+	  presets: ['babel-preset-expo'],
+	  plugins: [
+		[
+		  'module-resolver',
+		  {
+			root: ['./src'],
+			alias: {
+			  '@assets': './src/assets',
+			  '@components': './src/components',
+			  '@screens': './src/screens',
+			  '@storage': './src/storage',
+			  '@theme': './src/theme',
+			  '@utils': './src/utils',
+			}
+		  }
+		]
+	  ]
 	};
-};
-
+  };
 
 //aqui só coloquei essas pastas como alias pro import ficar mais limpo e organizado (fácil)
--- igniteTeams/src/theme/index.ts
+++ igniteTeams/src/theme/index.ts
@@ -1,4 +1,3 @@
-
 export default {
   COLORS: {
     WHITE: '#FFFFFF',
